a21dba33094a872380df00942deaf172c5886577
aleem ahmed <w3st.io2021@gmail.com>
2022-08-10 15:13:53 -0400

    s

---
+++ crypto-analyzer/src/main/java/com/cryptoanalyzer/Main.java
@@ -0,0 +1,13 @@
+package com.cryptoanalyzer;
+
+public class Main {
+    int a = 30;
+    long b = 1_234_567_890;
+    float p = 10.99F;
+    char letter = 'A';
+    boolean tf = true;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
---
+++ hello_world.java
@@ -0,0 +1,9 @@
+public class hello_world {
+	private static void printHelloWorld() {
+		System.out.println("Hello World!!");
+	}
+
+	public static void main(String args[]) {
+		printHelloWorld();
+	}
+}
