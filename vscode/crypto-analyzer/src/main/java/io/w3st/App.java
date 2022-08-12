fc14a04a9c96082eeb9560c6782f2ad7f54ff8d9
aleem ahmed <w3st.io2021@gmail.com>
2022-08-12 15:26:32 -0400

    s

--- vscode/crypto-analyzer/src/main/java/io/w3st/App.java
+++ vscode/crypto-analyzer/src/main/java/io/w3st/App.java
@@ -3,15 +3,22 @@ import static spark.Spark.*;
 
 /**
  * @title App
- */
+*/
 public final class App {
 	/**
-	 * @param args The arguments of the program.
-	 */
+	 * @param args The arguments of the program
+	*/
 	public static void main(String[] args) {
 		get("/api", (req, res) -> {
 			res.type("application/json");
-			return "{\"message\":\"Custom 404\"}";
+
+			return "{ \"executed\": true, \"status\": true }";
+		});
+
+		get("/*", (req, res) -> {
+			res.type("application/json");
+
+			return "{ \"executed\": true, \"status\": false, \"message\":\"Custom 404\" }";
 		});
 	}
 }
