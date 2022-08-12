2d2edc9e2501bd4f7b9b0f411676b565a67bd621
aleem ahmed <w3st.io2021@gmail.com>
2022-08-12 15:17:38 -0400

    Building out the REST API

--- vscode/crypto-analyzer/src/main/java/io/w3st/App.java
+++ vscode/crypto-analyzer/src/main/java/io/w3st/App.java
@@ -5,10 +5,13 @@ import static spark.Spark.*;
  * @title App
  */
 public final class App {
-    /**
-     * @param args The arguments of the program.
-     */
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
-    }
+	/**
+	 * @param args The arguments of the program.
+	 */
+	public static void main(String[] args) {
+		get("/api", (req, res) -> {
+			res.type("application/json");
+			return "{\"message\":\"Custom 404\"}";
+		});
+	}
 }
